681f895c5d8dcce250829a5fdaf94f99da89ace4
chokiproai <Dogaopc13@gmail.com>
2023-10-16 22:00:59 +0700

    ChatGPT-Plugins

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "chokiproai";
-export const REPO = "chatgpt";
+export const REPO = "ChatGPT-Plugins";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;

b140d82f499e1ec0c526719c7f37479cab13b857
chokiproai <Dogaopc13@gmail.com>
2024-09-14 21:05:28 +0700

    [v2.14.8] Fix vi

--- app/masks/build.ts
+++ app/masks/build.ts
@@ -6,7 +6,7 @@ import { EN_MASKS } from "./en";
 import { type BuiltinMask } from "./typing";
 
 const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
-  cn: VI_MASKS,
+  vi: VI_MASKS,
   en: EN_MASKS,
 };
 

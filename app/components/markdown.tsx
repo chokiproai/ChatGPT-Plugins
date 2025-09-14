151ed639041337c6fa2ea77e26598a0c54bade5e
chokiproai <dogaopc13@gmail.com>
2025-09-14 15:17:03 +0700

    rehype-katex 7.0.0

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -293,7 +293,7 @@ function MarkDownContent(props: {
     <ReactMarkdown
       remarkPlugins={[RemarkMath, RemarkGfm, RemarkBreaks]}
       rehypePlugins={[
-        RehypeKatex as any,
+        RehypeKatex,
         [
           RehypeHighlight,
           {
